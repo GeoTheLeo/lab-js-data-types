52f0e187892b51df741b75b803b82d725fa8ee4b
GeoTheLeo <professorgeotheleo@outlook.com>
2026-02-02 17:34:41 +0100

    fun JAVA bits

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
+const tongueTwister = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5;
 
 
 // Print out the concatenated string
+console.log(tongueTwister);
+
+// more elegant, modern
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5}`;
+console.log(tongueTwister);
 
 
 
@@ -22,9 +28,13 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+const camelTail =
+  part1.slice(0, -1) + part1.slice(-1).toUpperCase() +
+  part2.slice(0, -1) + part2.slice(-1).toUpperCase();
 
 
 // Print the cameLtaiL-formatted string
+console.log(camelTail);
 
 
 
@@ -35,9 +45,14 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
+const tipAmount = billTotal * 0.15;
+
+// a little extra
+console.log(`€${tipAmount.toFixed(2)}`);
 
 
 // Print out the tipAmount
+console.log(tipAmount);
 
 
 
@@ -47,9 +62,11 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 
 
 // Print the generated random number
+console.log(randomNumber);
 
 
 
@@ -61,16 +78,16 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b;      // false
 
-const expression2 = a || b;
+const expression2 = a || b;      // true
 
-const expression3 = !a && b;
+const expression3 = !a && b;     // false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b);   // true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b;    // true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b);   // false
 
-const expression7 = a && a;
+const expression7 = a && a;      // true
